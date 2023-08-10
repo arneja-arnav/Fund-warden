0ff2e3c3bf3dd5d6c8273aba8339e5f8de2085ea
arneja-arnav <arnavdeepsingharneja@gmail.com>
2023-08-10 22:10:16 +0530

    added frontend files

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- Include the LandingPage component -->
+    <LandingPage />
+  </div>
+</template>
+
+<script>
+// Import the LandingPage component
+import LandingPage from './components/landing_page.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // Register the LandingPage component
+    LandingPage
+  }
+};
+</script>
+
+<style>
+/* Add your global app styles here */
+</style>
+
---
+++ frontend/src/components/landing_page.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="landing-page">
+      <header>
+        <h1 class="app-name">{{ appName }}</h1>
+        <p class="slogan">{{ slogan }}</p>
+      </header>
+      <section class="main-content">
+        <p class="description">
+          Welcome to Fund Warden, your comprehensive financial planning app that helps you maximize your earning potential.
+        </p>
+        <router-link to="/register" class="cta-button">Get Started</router-link>
+      </section>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LandingPage',
+    data() {
+      return {
+        appName: 'Fund Warden',
+        slogan: 'Maximize your earning potential using Fund Warden'
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your CSS styling here */
+  </style>
+  
